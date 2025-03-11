39857ce9e7167c2b249b74757ae808483b1cd472
AMIT <amitsinghit0@gmail.com>
2025-03-12 02:43:10 +0530

    12 march added

---
+++ src/ex_01_Java_Basic/NineTable.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+public class NineTable {
+    public static void main(String[] args){
+    int num = 9;
+    System.out.println("9x1 = " + num*1);
+    System.out.println("9x2 = " + num*2);
+    System.out.println("9x3 = " + num*3);
+    System.out.println("9x4 = " + num*4);
+    System.out.println("9x5 = " + num*5);
+    System.out.println("9x6 = " + num*6);
+    System.out.println("9x7 = " + num*7);
+    System.out.println("9x8 = " + num*8);
+    System.out.println("9x9 = " + num*9);
+    System.out.println("9x10= "+ num*10);
+
+    }
+}
