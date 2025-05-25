a609622fe81603e9c91123ccd5c929fc51bc91fa
AMIT <amitsinghit0@gmail.com>
2025-05-26 00:15:27 +0530

    17 march added

---
+++ src/ex_010_For_Loop/Lab_097_For_Loop.java
@@ -0,0 +1,11 @@
+package ex_010_For_Loop;
+
+public class Lab_097_For_Loop {
+    public static void main(String[] args) {
+        for(int i =0;i<5;i++){
+            System.out.println(i);
+// this will print the from 0 to 4
+        }
+
+    }
+}
